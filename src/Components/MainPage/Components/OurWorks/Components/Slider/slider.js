6247b746564d24446d51391c1ce2d6e3091c7bb7
Evgeny <chiburashka47@gmail.com>
2020-09-17 12:07:57 +0300

    drawer opacity

--- src/Components/MainPage/Components/OurWorks/Components/Slider/slider.js
+++ src/Components/MainPage/Components/OurWorks/Components/Slider/slider.js
@@ -3,23 +3,32 @@ import getPhoto from "../../../../../../public/getPhoto";
 
 export default function Slider({ activeItem, navList }) {
   return (
-    <div className="slider">
-      <div className="slider__textContainer">
-        <span className="slider__textContainer-text">{activeItem.text}</span>
-      </div>
-      <div className="slider__photoContainer">
-        {navList.map((elem, index) => {
-          return (
-            <div className="slider__photoContainer__item">
-              <div className="slider__photoContainer__item-container">
-                <div key={index} className="slider__photoContainer__item-img">
-                  {getPhoto(`${activeItem.img}${index}`)}
-                </div>
-              </div>
+    <>
+      {navList.map((elem, index) => {
+        return (
+          <div className={`slider ${elem.active ? "active " : ""}`}>
+            <div className="slider__textContainer">
+              <span className="slider__textContainer-text">{elem.text}</span>
             </div>
-          );
-        })}
-      </div>
-    </div>
+            <div className="slider__photoContainer">
+              {navList.map((elem2, index) => {
+                return (
+                  <div className="slider__photoContainer__item">
+                    <div className="slider__photoContainer__item-container">
+                      <div
+                        key={index}
+                        className="slider__photoContainer__item-img"
+                      >
+                        {getPhoto(`${elem.img}${index}`)}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
